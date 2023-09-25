8f1e08a2a92c54b11646b0daaf0b92519aa551ae
Jawid-Yari <jawidyari@gmail.com>
2023-09-25 22:33:34 +0200

    dataclass

--- banksystem.py
+++ banksystem.py
@@ -1,25 +1,33 @@
+from dataclasses import dataclass
 import datetime 
 import random
 from time import perf_counter
 from barnum import gen_data
-class Customer:
-    def __init__(self, name, birthdate, account_number, created,saldo, last_updated: str = None):
-        self.name = name 
-        self.birthdate = birthdate
-        self.account_number = account_number
-        self.created = created
-        self.saldo = saldo
-        self.last_updated = last_updated
 
-    def __repr__(self) -> str:
-        return (f"Account owner: {self.name}\n"
+
+
+@dataclass
+class Customer:
+    name: str
+    birthdate:str 
+    account_number: str 
+    created: datetime
+    saldo: float
+    last_updated:datetime
+
+
+    """
+    Jag vill formatera klassens output attribut
+    """
+    def __str__(self) -> str:
+        formatted_output = (f"Account Owner: {self.name}\n"
                 f"Birthdate: {self.birthdate}\n"
                 f"Account number: {self.account_number}\n"
                 f"Created date: {self.created}\n"
                 f"Saldo: {self.saldo}\n"
-                f"Last Updated: {self.last_updated}\n"
+                f"Last updated: {self.last_updated}\n"
                 )
-
+        return formatted_output
 
 
 def count_time(message):
@@ -28,35 +36,53 @@ def count_time(message):
             start = perf_counter()
             result =func(*args, **kwargs)
             end = perf_counter()
-            print( f"{message} took {end - start} seconds")
+            print( f"It took {end - start} seconds {message}")
             return result
         return inner
     return decorator
     
 
-
-
+@dataclass
 class Customer_database: 
-    def __init__(self):
-        self.customers = []
-        self.generated_account_number = set()
+    customers = []
+    generated_account_number = set()
 
     def __repr__(self) -> str:
         return self.customers
     
-    def quicksort(self, customers):
-        if len(customers) <= 1:
-            return customers
-        else:
-            pivot = customers[0].account_number[-10:]
-            smaller = [customer for customer in customers if customer.account_number[-10:] < pivot]
-            equal = [customer for customer in customers if customer.account_number[-10:] == pivot]
-            larger = [customer for customer in customers if customer.account_number[-10:] > pivot]
-            return self.quicksort(smaller) + equal + self.quicksort(larger)
+    def partition(self, list, start, end):
+        pivot = int(list[start].account_number[-10:])
+        low = start +1
+        high = end
+        while True:
+            while low <= high and int(list[high].account_number[-10:]) >= pivot:
+                high = high -1
+            while low <= high and int(list[low].account_number[-10:]) <= pivot:
+                low = low + 1 
+            if low <= high:
+                list[low], list[high] = list[high], list[low]
+            else:
+                break
+        list[start], list[high] = list[high], list[start]
+
+        return high
+    
+    
+    def quick_sort(self, array, start, end):
+        if start >= end:
+            return
+        p = self.partition(array, start, end)
+        self.quick_sort(array, start, p-1)
+        self.quick_sort(array, p+1, end)
+    
 
     
     def sort_customers_by_account_number(self):
-        self.customers = self.quicksort(self.customers)
+        customers_list = self.customers
+        self.quick_sort(customers_list, 0, len(customers_list) - 1)
+        self.customers = customers_list
+
+
 
 
     @count_time("Searching for account number")
@@ -72,9 +98,8 @@ class Customer_database:
             elif number_to_search > lista_of_account_numbers[medium]:
                 low = medium + 1
             elif number_to_search == lista_of_account_numbers[medium]:
-                print(f"Account number '{element}' matchar {lista_of_account_numbers[medium]}")
-                break
-    
+                return(f"Account number '{element}' matchs {lista_of_account_numbers[medium]}")
+                
 
     def generate_account_num(self, c):
         account_number = f"1111-{c:00000000010}"
@@ -105,28 +130,20 @@ class Customer_database:
 
 
 
-
-    def get_account(self, account_to_search: str) -> Customer or None:
-        for customer in self.customers:
-            if customer.account_number == account_to_search:
-                return customer
-            
-            return None
-
-
 @count_time("Creating customer")
 def generate_customers():
-    customer_db.generate_customer(100)
+    customer_db.generate_customer(10_000_000)
 
 
 
-@count_time("Searching")    
+    
 def search_account(account_to_check: str):
-    found_account = customer_db.get_account(account_to_search=account_to_check)
-    if found_account:
-        print(f"Account number {account_to_check} belongs to:'{found_account.name}' ")
-    else:
-        print(f"Account number: {account_to_check} not found")
+    for itam in account_to_check:
+        found_account = customer_db.binary_search(element=itam)
+        if found_account:
+            print(found_account)
+        else:
+            print(f"Account number: {itam} not found")
 
 
 
@@ -142,8 +159,7 @@ if __name__ == "__main__":
     customer_db.sort_customers_by_account_number()
 
     accounts_to_check = ["1111-0000001000", "1111-0009999999", "1111-9999999999"]
-    for item in (accounts_to_check):
-        customer_db.binary_search(item)
+    search_account(account_to_check= accounts_to_check)
 
     
     for customer in customer_db.customers:
